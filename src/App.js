a267041af9c9d6654f1e6ff92f8d7c468c68c765
Deepak026 <deepakprofessional026@gmail.com>
2024-02-16 15:05:01 +0530

    first

--- src/App.js
+++ src/App.js
@@ -1,25 +1,36 @@
-import logo from './logo.svg';
+import React from 'react';
 import './App.css';
+import SpeechRecognition, { useSpeechRecognition } from 'react-speech-recognition';
+import { FaMicrophone } from "react-icons/fa";
+import { IoStopCircleOutline } from "react-icons/io5";
 
-function App() {
+const App = () => {
+  const {
+    transcript,
+    listening,
+    resetTranscript,
+    browserSupportsSpeechRecognition
+  } = useSpeechRecognition();
+
+  if (!browserSupportsSpeechRecognition) {
+    return <span>Browser doesn't support speech recognition.</span>;
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className='container'>
+      <div className='row'>
+        <div className='col'>
+        <h2>Microphone : {listening ? 'on' : 'off'}</h2>
+        <h2>Please Start Button and Say Hello !</h2>
+      <button className='btn btn-primary btm' onClick={SpeechRecognition.startListening}> <FaMicrophone/>Start</button>
+      <button className='btn btn-danger btm' onClick={SpeechRecognition.stopListening}> <IoStopCircleOutline/>Stop</button>
+      <button className='btn btn-warning btm' onClick={resetTranscript}>Reset</button>
+      <p className='script'>{transcript}</p>
+
+        </div>
+      </div>
+      
     </div>
   );
-}
-
+};
 export default App;
